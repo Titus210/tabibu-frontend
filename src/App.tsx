21cea9b46bbb7e7695c1d588c0819a6ab044ce27
devCoyo <tituskiplagat06@gmail.com>
2024-08-21 17:26:19 +0300

    feat: files setup and initial folder structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import AppRoutes from "./routes/AppRoutes"
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center  mx-auto ">
+          <AppRoutes/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
+
+import GetStarted from "../components/pages/common/get-started/GetStarted"
+
+
+const AppRoutes = () => {
+    return (
+      <Router>
+  
+        {/*Common Routes */}
+        <Routes>
+          <Route path="/" element={<GetStarted />} />
+        </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes
