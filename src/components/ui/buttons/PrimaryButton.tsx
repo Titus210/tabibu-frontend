8990147f48dd7745e1c87c31535aea96685519f7
devCoyo <tituskiplagat06@gmail.com>
2024-08-24 21:55:16 +0300

    feat: Add disabled state to button
    - Add 'disabled' class to button on click
    - Include styles for opacity when button is disabled

--- src/components/ui/buttons/PrimaryButton.tsx
+++ src/components/ui/buttons/PrimaryButton.tsx
@@ -9,21 +9,24 @@ export interface ButtonProps {
     pageLink?: string;
     onClick?: () => void;
     type?: "submit" | "reset" | "button";
+    disabled?: boolean; 
 }
 
-const PrimaryButton: React.FC<ButtonProps> = ({ buttonName, bgColor, onClick, type, textColor , pageLink}) => {
+const PrimaryButton: React.FC<ButtonProps> = ({ buttonName, bgColor, onClick, type, textColor, pageLink, disabled }) => {
 
     const backgroundClass = bgColor ? `bg-${bgColor}` : 'bg-blue';
-    const hoverClass = bgColor ? `hover:bg-${bgColor}` : 'hover:bg-blue';
+    const hoverClass = disabled ? '' : (bgColor ? `hover:bg-${bgColor}` : 'hover:bg-blue');
     const textClass = textColor ? `text-${textColor}` : 'text-white';
+    const disabledClass = disabled ? 'opacity-50 cursor-not-allowed' : '';
 
     return (
         <div className='flex items-center justify-center'>
-            <Link to={pageLink || '#'}>
+            <Link to={disabled ? '#' : (pageLink || '#')}>
                 <button
-                    className={`${backgroundClass} ${hoverClass} ${textClass} font-bold py-2 px-4 rounded-md w-full md:w-auto`}
-                    onClick={onClick}
+                    className={`${backgroundClass} ${hoverClass} ${textClass} ${disabledClass} font-bold py-2 px-4 rounded-md w-full md:w-auto`}
+                    onClick={disabled ? undefined : onClick} // Disable click when the button is disabled
                     type={type || "button"}
+                    disabled={disabled}
                 >
                     {buttonName || 'Submit'}
                 </button>
